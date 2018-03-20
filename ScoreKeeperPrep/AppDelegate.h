4e799d7294ac31ca81aad2d37d9d142dc94e6083
Joshua Laesch <joshua.laesch@gmail.com>
2018-03-20 12:05:04 -0400

    Initial Commit

---
+++ ScoreKeeperPrep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoreKeeperPrep
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
